5b96a60e304a2fe18f35685e9032cf590f9e434a
Sodiq Yusuff <ysodiqakanni@gmail.com>
2025-08-23 17:25:37 -0500

    Add project files.

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // CRUD -> Create, Read, Update, Delete
+
+        // Index Action Method
+        // Use this to display a list of students
+        // Route:  /Students/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // An action method for creating a new student
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Readme.md
@@ -0,0 +1,9 @@
+# Assignment
+1. When the app is running and we make changes to the view, the changes should be reflected immediately without needing to restart the app.
+2. Create a link to go to the student's list page. The link should be after the Privacy link.
+	Hint: the existing links are in the layout view.
+3. Add a "Create" button on top of students list table. The button should link to the students/create page.
+4. Add a details button to each element in the list. When clicked should open the details page.
+
+
+5. Do the same for the teacher's module. Create, List, Edit, details, delete
