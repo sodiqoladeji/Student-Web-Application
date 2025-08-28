c910ea842d0bdb6b524336da6f9ddf8de1a928a3
sodiqoladeji <sodiqoladeji69@gmail.com>
2025-08-28 19:35:56 +0100

    Students Controller Updated Teachers Controller created

---
+++ WebApplication1/Controllers/TeachersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TeachersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
