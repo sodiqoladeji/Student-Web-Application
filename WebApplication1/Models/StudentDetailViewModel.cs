809ae9cae5149a662c07780cf66e98c53579360a
sodiqoladeji <sodiqoladeji69@gmail.com>
2025-11-07 23:42:44 +0000

    Extra Fields

--- WebApplication1/Models/StudentDetailViewModel.cs
+++ WebApplication1/Models/StudentDetailViewModel.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 namespace WebApplication1.Models
 {
     public class StudentDetailViewModel
     {
         public int Id { get; set; }
-        [Required (ErrorMessage = "The Name field is Required")]
-        public string Name { get; set; }
+
+        [Required (ErrorMessage = "The FirstName field is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The LastName field is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The DateofBirth field is Required")]
+        [DataType(DataType.Date)]
+        public DateOnly DateofBirth{ get; set; }
+
+        [Required(ErrorMessage = "The Gender field is Required")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "The CountryofBirth field is Required")]
+        [MaxLength(56, ErrorMessage = "Country of Birth cannot exceed 56 characters")]
+        public string CountryofBirth { get; set; }
+
+        [Required(ErrorMessage = "The PhoneNumber field is Required")]
+        [MaxLength(11, ErrorMessage = "Invalid Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
 
         [Required (ErrorMessage = "Email is Required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Address field is Required")]
+        public string Address { get; set; }
 
         [Required(ErrorMessage = "The Enrollment field is Required")]
         [DataType(DataType.Date)]
